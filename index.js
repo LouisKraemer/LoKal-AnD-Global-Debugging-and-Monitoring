7818880d8898cdbb110d783d9180d58cec6c6710
Louis Kraemer <louiskraemer@gmail.com>
2024-03-07 11:58:35 +0000

    remove ngrok from package.json

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const ngrok = require('ngrok');
-
-const initNgRock = async () => {
-  const url = await ngrok.connect();
-  console.log(`Local server exposed on ${url}`);
-};
-
-initNgRock().catch((error) => {
-  console.error(error);
-  ngrok.disconnect();
-})
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.json());
 
-server.post('/', function(request, response) {
+server.post("/", function (request, response) {
   console.log(request.body);
   response.status(200).send();
 });
